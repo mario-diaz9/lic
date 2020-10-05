1fc857873959603ce439223d54a6a3b27b38cd22
Mario Daniel <mario.daniel.diaz.a@gmail.com>
2020-10-04 21:08:20 -0600

    ejercicio 2

--- guia 10/ejercicio2/js/script.js
+++ guia 10/ejercicio2/js/script.js
@@ -1,15 +1,15 @@
 prettyPrint();
 
 $('#ascensorBuilding').ascensor({
-	AscensorName:'ascensor',
-	ChildType:'section',
-	AscensorFloorName:'Home | Implementation | HTML | Jquery | CSS | Smartphone | Credits| Prueba',
-	Time:1000,
-	WindowsOn:1,
-	Direction:'chocolate', 
-    AscensorMap:'2|1 & 2|2 & 3|2 & 2|3 & 2|4 & 1|4 & 2|5',
-    Easing:'easeInOutQuad',
-    KeyNavigation:true,
-    Queued:false,
- 
+    AscensorName: 'ascensor',
+    ChildType: 'section',
+    AscensorFloorName: 'Home | que | positivos | que2 | Tipos | wha | Tipos2 | insta | face | negativos',
+    Time: 1000,
+    WindowsOn: 1,
+    Direction: 'chocolate',
+    AscensorMap: '2|1 & 2|2 & 3|3 & 2|3 & 2|4 & 1|4 & 2|5 & 1|3 & 1|2 & 3|4',
+    Easing: 'easeInOutQuad',
+    KeyNavigation: true,
+    Queued: false,
+
 });
